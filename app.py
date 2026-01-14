b5cb66bdf068b1024ea26000b0b7aef3d3d24417
shageer atar <shageeratar@gmail.com>
2026-01-14 23:21:40 +0530

    Fix syntax error

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 @app.route("/")
 def home():
-    return """
-    <h1>AI Sleep Monitoring Website</h1>
-    <p>Status: Running Successfully</p>
-    """
+    return "AI Sleep Monitoring Website is LIVE"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=10000
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=10000)
